cb28ded631df8d8b3b4d6c032fe29239cb992de3
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-05-28 19:53:45 +0300

    9ffed9e

---
+++ index.88b7e518.js
@@ -0,0 +1,9 @@
+(function () {
+var $933923e164b0c663$exports = {};
+// import * as classes from "./style.module.scss";
+$(document).ready(()=>{
+    $("body").html("Hello World, if you see this message Parcel Bundler and Github Hosting are working nicely!");
+});
+
+})();
+//# sourceMappingURL=index.88b7e518.js.map
---
+++ index.e4561611.js
@@ -0,0 +1,8 @@
+var $b9dff960cd1c7100$exports = {};
+// import * as classes from "./style.module.scss";
+$(document).ready(()=>{
+    $("body").html("Hello World, if you see this message Parcel Bundler and Github Hosting are working nicely!");
+});
+
+
+//# sourceMappingURL=index.e4561611.js.map
