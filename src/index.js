e32b672724d8e854f0e470139593f239481fde7c
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-05-28 19:40:47 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 // import * as classes from "./style.module.scss";
+$(document).ready(() => {
+  $("body").html(
+    "Hello World, if you see this message Parcel Bundler and Github Hosting are working nicely!"
+  );
+});
